cd63cdaad15defa27dbed71b2f5a99e35c4cad87
niels <niels@hostmatters.nl>
2018-04-06 16:37:03 +0200

    …

--- themeinsert.php
+++ themeinsert.php
@@ -1,32 +1,32 @@
 <?php
 /**
  * Plugin Name: Theme Insert
- * Plugin URI: 
+ * Plugin URI:
  * Description: .
- * Version: 1.2
- * Author: Niels	
- * Author URI: 
+ * Version: 1.3
+ * Author: Niels
+ * Author URI:
  */
- 
+
 
 	function footerfields_create() {
 	    $page_title = 'Theme-Insert';
 	    $menu_title = 'Theme-Insert';
-	    $capability = 'edit_posts';
+	    $capability = 'manage_options';
 	    $menu_slug = 'themeinsert';
 	    $function = 'footerfields_form';
 	    $icon_url = '';
 	    $position = 94;
-	
+
 	    add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
 	}
-	add_action('admin_menu', 'footerfields_create'); 
+	add_action('admin_menu', 'footerfields_create');
 
 	 function footerfields_init(){
 			add_option( 'headerfields', '', '', 'yes' );
-			add_option( 'footerfields', '', '', 'yes' ); 
+			add_option( 'footerfields', '', '', 'yes' );
 		}
-	register_activation_hook(__FILE__,'footerfields_init');	
+	register_activation_hook(__FILE__,'footerfields_init');
 
 
 	function footerfields_destroy(){
@@ -35,15 +35,19 @@
 		}
 	register_deactivation_hook( __FILE__, 'footerfields_destroy' );
 
- 
+
 	 function footerfields_form(){
-		    if (isset($_POST['headerfields'])) {
-			   	update_option('headerfields', $_POST['headerfields']); 
+		 	/* restrict normal users */
+		 	if ( !current_user_can( 'manage_options' ) )  {
+		 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 			}
-		    
+		    if (isset($_POST['headerfields'])) {
+			   	update_option('headerfields', $_POST['headerfields']);
+			}
+
 		    if (isset($_POST['footerfields'])) {
 		        update_option('footerfields', $_POST['footerfields']);
-		    } 
+		    }
 
 		    if ($_POST){
 			    footerfields_notice__success();
@@ -59,20 +63,27 @@
 	    </div>
 	    <?php
 	}
-	
+
 	function footerfields_headtags (){
 		if (get_option( 'headerfields' ) != ""){
 			echo wp_unslash(get_option( 'headerfields' ));
 		}
 	}
 	add_action('wp_head', 'footerfields_headtags', 10);
-	
+
 	function footerfields_footertags (){
-		if (get_option( 'footerfields' ) != ""){	
+		if (get_option( 'footerfields' ) != ""){
 			echo wp_unslash(get_option( 'footerfields' ));
 		}
 	}
 	add_action('wp_footer', 'footerfields_footertags', 10);
-	
-		
-	
+
+
+
+	/* Update from github if any */
+	require 'plugin-update-checker/plugin-update-checker.php';
+		$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+			'https://github.com/hostmatters/Theme-insert/',
+			__FILE__,
+			'Test-Auto-update'
+		);
